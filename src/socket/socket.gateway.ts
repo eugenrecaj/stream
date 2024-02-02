e8bf32537a508858ce6e46c6efe2ab1e9931c7cc
eugenrecaj <geni.recaj11@gmail.com>
2024-02-02 12:18:38 +0100

    test

--- src/socket/socket.gateway.ts
+++ src/socket/socket.gateway.ts
@@ -28,39 +28,52 @@ export class ChatGateway
   users = {};
   socketToRoom = {};
 
+  handleConnection(client: Socket) {
+    console.log(`Client connected: ${client.id}`);
+  }
+
   handleDisconnect(client: Socket) {
+    console.log(`Client disconnected: ${client.id}`);
     const roomID = this.socketToRoom[client.id];
     let room = this.users[roomID];
     if (room) {
       room = room.filter((id) => id !== client.id);
       this.users[roomID] = room;
+      if (room.length > 0) {
+        // Notify remaining users that someone has disconnected
+        this.server.to(roomID).emit('user disconnected', client.id);
+      } else {
+        // If no users left in the room, delete the room
+        delete this.users[roomID];
+      }
     }
+    delete this.socketToRoom[client.id];
   }
 
   @SubscribeMessage('join room')
-  handleJoinRoom(data, socket) {
-    console.log(data);
-
-    const roomID = data.room;
-
+  handleJoinRoom(client: Socket, roomID: string) {
     if (this.users[roomID]) {
       const length = this.users[roomID].length;
       if (length === 4) {
-        socket.emit('room full');
+        client.emit('room full');
         return;
       }
-      this.users[roomID].push(socket.id);
+      this.users[roomID].push(client.id);
     } else {
-      this.users[roomID] = [socket.id];
+      this.users[roomID] = [client.id];
     }
-    this.socketToRoom[socket.id] = roomID;
-    const usersInThisRoom = this.users[roomID].filter((id) => id !== socket.id);
+    this.socketToRoom[client.id] = roomID;
+    const usersInThisRoom = this.users[roomID].filter((id) => id !== client.id);
 
-    this.server.emit('all users', usersInThisRoom);
+    client.emit('all users', usersInThisRoom);
+    client.join(roomID);
   }
 
   @SubscribeMessage('sending signal')
-  handleSendingSignal(payload, client) {
+  handleSendingSignal(
+    client: Socket,
+    payload: { userToSignal: string; signal: any; callerID: string },
+  ) {
     this.server.to(payload.userToSignal).emit('user joined', {
       signal: payload.signal,
       callerID: payload.callerID,
@@ -68,23 +81,16 @@ export class ChatGateway
   }
 
   @SubscribeMessage('returning signal')
-  handleReturningSignal(payload, client) {
+  handleReturningSignal(
+    client: Socket,
+    payload: { callerID: string; signal: any },
+  ) {
     this.server.to(payload.callerID).emit('receiving returned signal', {
       signal: payload.signal,
       id: client.id,
     });
   }
-
   afterInit(server: Server) {
     console.log('Initialized!');
-  }
-
-  handleConnection(client: Socket, ...args: any[]) {
-    const room = client.handshake.auth.room;
-    console.log(room.toString());
-
-    if (room) {
-      client.join(room.toString());
-    }
   }
 }
