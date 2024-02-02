2c0197597eea10ae73d6a4fa6900d9cfd65381e6
eugenrecaj <geni.recaj11@gmail.com>
2024-02-02 13:19:57 +0100

    test multiple

--- src/socket/socket.gateway.ts
+++ src/socket/socket.gateway.ts
@@ -24,6 +24,7 @@ export class ChatGateway
 {
   constructor() {}
   @WebSocketServer() server: Server;
+  users = {};
 
   afterInit(server: Server) {
     console.log('WebSocket Server initialized');
@@ -34,24 +35,44 @@ export class ChatGateway
   }
 
   handleDisconnect(client: Socket) {
+    const roomID = this.users[client.id];
+    if (roomID) {
+      client.to(roomID).emit('user left', client.id);
+      client.leave(roomID);
+      this.server.sockets.emit(
+        'update users',
+        Object.keys(this.users).filter((socketId) => socketId !== client.id),
+      );
+      delete this.users[client.id];
+    }
     console.log(`Client disconnected: ${client.id}`);
   }
 
+  @SubscribeMessage('join room')
+  joinRoom(client: Socket, room: string): void {
+    this.users[client.id] = room;
+    client.join(room);
+    this.server.to(room).emit(
+      'update users',
+      Object.keys(this.users).filter(
+        (socketId) => this.users[socketId] === room,
+      ),
+    );
+    console.log(`Client ${client.id} joined room: ${room}`);
+  }
+
   @SubscribeMessage('offer')
-  handleOffer(client: Socket, payload: string): void {
-    console.log('Offer received and broadcasted');
-    client.broadcast.emit('offer', payload);
+  handleOffer(client: Socket, { offer, to }): void {
+    this.server.to(to).emit('offer', { offer, from: client.id });
   }
 
   @SubscribeMessage('answer')
-  handleAnswer(client: Socket, payload: string): void {
-    console.log('Answer received and broadcasted');
-    client.broadcast.emit('answer', payload);
+  handleAnswer(client: Socket, { answer, to }): void {
+    this.server.to(to).emit('answer', { answer, from: client.id });
   }
 
   @SubscribeMessage('candidate')
-  handleCandidate(client: Socket, payload: string): void {
-    console.log('ICE Candidate received and broadcasted');
-    client.broadcast.emit('candidate', payload);
+  handleCandidate(client: Socket, { candidate, to }): void {
+    this.server.to(to).emit('candidate', { candidate, from: client.id });
   }
 }
